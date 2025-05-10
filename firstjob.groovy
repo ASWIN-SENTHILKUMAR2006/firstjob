2d2780cd114e6cb09b05590203a69c1670fd1a96
Aswin Senthilkumar <aswinsenthilkumarcse@gmail.com>
2025-05-10 09:57:36 +0530

    Create firstjob.groovy

---
+++ firstjob.groovy
@@ -0,0 +1,26 @@
+pipeline {
+agent any
+stages {
+stage('Checkout Code') {
+steps {
+git 'https://github.com/your-repo/sample-project.git'
+}
+MKCE_CSE Page 26
+}
+stage('Build') {
+steps {
+sh 'javac Main.java' // Example build step for Java
+}
+}
+stage('Test') {
+steps {
+sh 'java Main' // Running the compiled Java program
+}
+}
+stage('Deploy') {
+steps {
+echo 'Deploying application...'
+}
+}
+}
+}
